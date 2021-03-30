7f6073c51de73461721b7266b044b9cf880d8179
KySo4oK <prostorostuk08@gmail.com>
2021-03-30 20:17:52 +0300

    add db in another cluster and connect from user-service

--- src/main/java/micro/user/service/userservice/HelloController.java
+++ src/main/java/micro/user/service/userservice/HelloController.java
@@ -1,23 +1,39 @@
 package micro.user.service.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.time.Instant;
 
 @RestController
 @RequestMapping("/user")
 public class HelloController {
+    @Autowired
+    DataSource dataSource;
+
     private boolean fail;
+
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         if (fail) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>("Hello! User microservice is on Kubernetes now!" + Instant.now(), HttpStatus.OK);
+    }
+
+    @GetMapping("/db")
+    public String dbTest(){
+        try {
+            return dataSource.getConnection().getSchema();
+        } catch (Exception throwables) {
+            return "db not connected " + throwables.getMessage();
+        }
     }
 
     @GetMapping("/fail")
