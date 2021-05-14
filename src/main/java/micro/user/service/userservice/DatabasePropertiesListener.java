62d84b7cb7b5e356ccfaa4c55cbfb0ac466c933a
KySo4oK <prostorostuk08@gmail.com>
2021-05-14 23:58:41 +0300

    add base64 decoding secrets

--- src/main/java/micro/user/service/userservice/DatabasePropertiesListener.java
+++ src/main/java/micro/user/service/userservice/DatabasePropertiesListener.java
@@ -5,22 +5,30 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
 
+import java.util.Base64;
 import java.util.Map;
 import java.util.Properties;
 
 public class DatabasePropertiesListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
-    ConfigClient configClient = new ConfigClient();
+
+    private final ConfigClient configClient = new ConfigClient();
+
+    private Map<String, String> sources;
 
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
         ConfigurableEnvironment environment = event.getEnvironment();
         Properties props = new Properties();
-        Map<String, String> sources = configClient.getSources("user", "sccs");
+        sources = configClient.getSources("user", "sccs");
         props.put("spring.datasource.url", "jdbc:postgresql://postgres-service.default.svc.cluster.local/POSTGRES_DB");
         props.put("spring.datasource.driverClassName", "org.postgresql.Driver");
-        props.put("spring.datasource.username", sources.get("dbusername"));
-        props.put("spring.datasource.password", sources.get("dbpassword"));
-        props.put("spring.rabbitmq.password", sources.get("spring.rabbitmq.password"));
-        props.put("spring.rabbitmq.username", sources.get("spring.rabbitmq.username"));
+        props.put("spring.datasource.username", getSecret("dbusername"));
+        props.put("spring.datasource.password", getSecret("dbpassword"));
+        props.put("spring.rabbitmq.password", getSecret("spring.rabbitmq.password"));
+        props.put("spring.rabbitmq.username", getSecret("spring.rabbitmq.username"));
         environment.getPropertySources().addFirst(new PropertiesPropertySource("myProps", props));
+    }
+
+    private String getSecret(String key) {
+        return new String(Base64.getDecoder().decode(sources.get(key)));
     }
 }
