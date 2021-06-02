2d662e255782a85499b8d621c89f7e5b5524290c
KySo4oK <prostorostuk08@gmail.com>
2021-06-02 19:40:49 +0300

    try keycloack

--- src/main/java/micro/user/service/userservice/HelloController.java
+++ src/main/java/micro/user/service/userservice/HelloController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class HelloController {
+
     @GetMapping("/hello")
     public String sayHello() {
+        return "Hello! User microservice is on Kubernetes now!";
+    }
+
+    @GetMapping("/hello/secured")
+    public String sayHelloSecured() {
         return "Hello! User microservice is on Kubernetes now!";
     }
 }
