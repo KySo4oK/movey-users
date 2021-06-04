b9563b4ff662ab2e032a60f7a48627cdbe4dd622
KySo4oK <prostorostuk08@gmail.com>
2021-06-04 22:28:18 +0300

    add setting userId

--- src/main/java/micro/user/service/userservice/controller/SwipeController.java
+++ src/main/java/micro/user/service/userservice/controller/SwipeController.java
@@ -2,8 +2,10 @@ package micro.user.service.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import micro.user.service.userservice.entity.Swipe;
+import micro.user.service.userservice.entity.User;
 import micro.user.service.userservice.service.SwipeService;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,8 @@ public class SwipeController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     void saveSwipe(@RequestBody Swipe swipe) {
+        String userId = String.valueOf(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId());
+        swipe.setUserId(Long.parseLong(userId));
         log.info("saving swipe - " + swipe);
         swipeService.save(swipe);
     }
