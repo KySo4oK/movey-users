0375df5c81e2765bfef0b0f6fe3d10ceee31d882
ivanluchkin <id.luchkin@gmail.com>
2021-06-04 20:28:08 +0300

    add feign client

--- src/main/java/micro/user/service/userservice/UserServiceApplication.java
+++ src/main/java/micro/user/service/userservice/UserServiceApplication.java
@@ -11,10 +11,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableFeignClients
 @EnableDiscoveryClient
 public class UserServiceApplication {
 
